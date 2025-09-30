eeb461a6ef1240fa705bcfb7be54980349b65f40
mohanakash503-ctrl <mohanakash503@gmail.com>
2025-09-30 20:37:31 +0530

    index.js

--- Task 6/index.js
+++ Task 6/index.js
@@ -1,33 +1,33 @@
 document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault(); // prevent actual submission
+    e.preventDefault(); 
 
     // Get inputs
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
     let message = document.getElementById("message").value.trim();
 
-    // Error placeholders
+
     let nameError = document.getElementById("nameError");
     let emailError = document.getElementById("emailError");
     let messageError = document.getElementById("messageError");
     let successMsg = document.getElementById("successMsg");
 
-    // Reset errors
+    
     nameError.textContent = "";
     emailError.textContent = "";
     messageError.textContent = "";
     successMsg.textContent = "";
 
-    // Validation flags
+    
     let valid = true;
 
-    // Name validation
+    
     if (name === "") {
         nameError.textContent = "Name is required";
         valid = false;
     }
 
-    // Email validation with regex
+    
     let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
     if (email === "") {
         emailError.textContent = "Email is required";
@@ -37,15 +37,16 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
         valid = false;
     }
 
-    // Message validation
+    
     if (message === "") {
         messageError.textContent = "Message cannot be empty";
         valid = false;
     }
 
-    // If all valid
+
     if (valid) {
         successMsg.textContent = "Form submitted successfully ✅";
         document.getElementById("contactForm").reset();
     }
 });
+
